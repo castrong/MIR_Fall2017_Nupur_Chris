78e5ad7986aa95421ed32187611659005e1f71f2
cstrong <cstrong@cs.hmc.edu>
2017-11-03 01:18:23 -0700

    godowky alignment start

---
+++ Godowsky Alignment/DTW_And_Reconstruction.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pyximport; pyximport.install()
+import dtw
+
+cost = np.array([[5,9,9,9], [9,8,2,9], [9,9,5,3]], dtype=np.float64)
+
+dn = [1,1,2,1] # allowed steps along the rows
+dm = [1,2,1,3] # allowed steps along the cols
+dw = [1.0, 1.0, 2.0, 3.0] # weight of each step
+subsequence = True # do subsequence matching
+# create a dictionary that holds your parameters - you'll send this to the DTW function
+parameter = {'dn': dn, 'dm': dm, 'dw': dw, 'SubSequence': subsequence}
+
+[accumCost, steps] = dtw.DTW_Cost_To_AccumCostAndSteps(cost, parameter)
+[path, endCol, endCost] = dtw.DTW_GetPath(accumCost, steps, parameter)
