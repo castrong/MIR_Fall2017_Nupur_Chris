2f13edcd98ad982aac6df19b5510f06c3253b6bb
cstrong <cstrong@cs.hmc.edu>
2017-11-03 01:22:56 -0700

    update

--- Godowsky Alignment/DTW_And_Reconstruction.py
+++ Godowsky Alignment/DTW_And_Reconstruction.py
@@ -4,8 +4,8 @@ import dtw
 
 cost = np.array([[5,9,9,9], [9,8,2,9], [9,9,5,3]], dtype=np.float64)
 
-dn = np.array([1,1,2,1]) # allowed steps along the rows
-dm = np.array([1,2,1,3]) # allowed steps along the cols
+dn = np.array([1,1,2,1], dtype=np.uint32) # allowed steps along the rows
+dm = np.array([1,2,1,3], dtype=np.uint32) # allowed steps along the cols
 dw = np.array([1.0, 1.0, 2.0, 3.0]) # weight of each step
 subsequence = True # do subsequence matching
 # create a dictionary that holds your parameters - you'll send this to the DTW function
