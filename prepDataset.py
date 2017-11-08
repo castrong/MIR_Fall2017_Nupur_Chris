9b59b26d9e4ce773c5670f221f2ae5b5d59b2492
nupurb <nupurb@gmail.com>
2017-11-08 01:04:56 -0800

    Tried to prep dataset

--- prepDataset.py
+++ prepDataset.py
@@ -2,30 +2,41 @@ import mido
 import random
 from mido import Message, MidiFile, MidiTrack
 
-orig = MidiFile('Godowtsai Dataset/Mechanical Godowsky/godowsky_v1_chopin_op10_e01.mid')
 
-
-def printMidi(midiName):
-	midiToPlay = MidiFile(midiName)
+def printMidi(fileName):
+	midiToPlay = MidiFile(fileName)
 	for track in midiToPlay.tracks:
 	    for msg in track:
 	        print(msg)
 
-def onlynote(pitchShift, timeScale):
+def onlynote(inputFileName, outputFileName, pitchShift, timeShift, timeScale):
+	orig = MidiFile(inputFileName)
 	mid = MidiFile()
 
 	for tr in orig.tracks:
 		track = MidiTrack()
 		mid.tracks.append(track)
+#<meta message track_name name='Grand Piano' time=0>
+
+#		msg = Message()
+		msg = Message('note_on', note=0, velocity=0, time=timeShift)
+		track.append(msg)
+
 		for msg in tr:
-			msg.time = round(msg.time * timeScale)
 			if msg.type == 'note_on':
+				msg.time = round(msg.time * timeScale)
 				msg.note = msg.note + pitchShift
-			track.append(msg)
+				track.append(msg)
 
-	mid.save('Output Midi/godowsky_v1_chopin_op10_e01_only_note.mid')
+	print(mid.ticks_per_beat)
 
-onlynote(-12,1.333)
+	mid.save(outputFileName)
+
+onlynote('Godowtsai Dataset/Chopin/chopin_op10_e01.mid', 'Output Midi/chopin_op10_e01_no_tempo.mid', 0, 0,1)
+printMidi('Output Midi/chopin_op10_e01_no_tempo.mid')
+onlynote('Godowtsai Dataset/Godowsky/godowsky_v1_chopin_op10_e01.mid', 'Output Midi/godowsky_v1_chopin_op10_e01_no_tempo.mid', 0, 0, 1)
+
+onlynote('Godowtsai Dataset/Godowsky/godowsky_v1_chopin_op10_e01.mid', 'Output Midi/godowsky_v1_chopin_op10_e01_shifted.mid', -12, 357, 0.3495)
 
 #printMidi('Godowtsai Dataset/Mechanical Godowsky/godowsky_v1_chopin_op10_e01.mid')
-printMidi('Godowtsai Dataset/Possibly Mechanical Chopin/chopin_op10_e01.mid')
+#printMidi('Godowtsai Dataset/Chopin/chopin_op10_e01.mid')
